181ad7ed3617655d9a4e3664a89025ed0b44e84b
hjmtql <hjmtql@gmail.com>
2020-09-30 17:34:26 +0900

    id integer -> serial

--- schema/1_trans.sql
+++ schema/1_trans.sql
@@ -1,18 +1,18 @@
-create table trans.account (id integer primary key);
+create table trans.account (id serial primary key);
 
 create table trans.user (
-    id integer primary key,
+    id serial primary key,
     account_id integer references trans.account(id) unique not null,
     name varchar(255) not null
 );
 
 create table trans.project (
-    id integer primary key,
+    id serial primary key,
     name varchar(255) not null
 );
 
 create table trans.task (
-    id integer primary key,
+    id serial primary key,
     project_id integer references trans.project(id) not null,
     name varchar(255) not null
 );
