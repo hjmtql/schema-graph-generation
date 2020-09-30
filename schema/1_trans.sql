9d37027558fb1ab122c04c3e7a76ec4a7b38ecbf
hjmtql <hjmtql@gmail.com>
2020-09-30 15:39:58 +0900

    varchar 256 -> 255

--- schema/1_trans.sql
+++ schema/1_trans.sql
@@ -3,18 +3,18 @@ create table trans.account (id integer primary key);
 create table trans.user (
     id integer primary key,
     account_id integer references trans.account(id) unique not null,
-    name varchar(256) not null
+    name varchar(255) not null
 );
 
 create table trans.project (
     id integer primary key,
-    name varchar(256) not null
+    name varchar(255) not null
 );
 
 create table trans.task (
     id integer primary key,
     project_id integer references trans.project(id) not null,
-    name varchar(256) not null
+    name varchar(255) not null
 );
 
 create table trans.project_user (
